be3ec9d9dc11350d3c544964e400cfa19bc9efe0
Felix A. Taveras <felixataveras1@gmail.com>
2024-09-01 03:19:45 -0400

    saving, nav bar done

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
             <div className="container mx-auto flex justify-between items-center">
                 <Link href='/'>
                     <h1 className="text-4xl font-semibold">
-                        Luke <span className="text-accent">.</span></h1>
+                        Felix <span className="text-accent">.</span></h1>
                 </Link>
 
                 {/* \\{ desktop nav } */}
