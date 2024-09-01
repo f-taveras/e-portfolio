3150040e55476d577a865313bf0bf1def378e0be
Felix A. Taveras <felixataveras1@gmail.com>
2024-08-31 20:42:00 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: ["class"],
+	content: [
+	  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	prefix: "",
+	theme: {
+	  container: {
+		center: true,
+		padding: '15px',
+	  },
+	  screens: {
+		sm: '640px',
+		md: '768px',
+		lg: '1024px',
+		xl: '1280px',
+		'2xl': '1536px',
+	  },
+	  fontFamily: {
+		primary: 'var(--font-jetbrains-mono)',
+	  },
+	  extend: {
+		colors: {
+		  primary: '#1c1c22',
+		  accent: {
+			DEFAULT: '#00ff99',
+			hover: '#00e187',
+		  },
+		},
+	  },
+	},
+	plugins: [require("tailwindcss-animate")],
+  };
+  
