73ea9c2c948784cda72bdd557f30855c2393f87a
Felix A. Taveras <felixataveras1@gmail.com>
2024-09-02 09:08:46 -0400

    nav bar, the animation is not taking

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,11 @@
 import { JetBrains_Mono } from "next/font/google";
+
 import "./globals.css";
 
 // components
 import Header from "../components/Header";
+import PageTransition from "../components/PageTransition";
+// import StairTransition from "@components/StairTransition";
 
 const JetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -19,7 +22,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={JetBrainsMono.variable}>
         <Header />
-        {children}
+        {/* <StairTransition /> */}
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
