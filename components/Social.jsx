e815cd51649e93e90f159a60de691d77545778bf
Felix A. Taveras <felixataveras1@gmail.com>
2024-09-06 19:25:18 -0400

    fixing social

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,23 +1,45 @@
-const Social = () => {
+import Link from 'next/link';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+
+socials = [
+    {
+        icon: <FaLinkedin />,
+        
+    }
+]
+
+const Social = ({containerStyles, iconStyles}) => {
     return (
 
-        <div className=' w-full h-34 flex gap-4'>
+        <div className={containerStyles}>
+            {socials.map((item, index) => {
+                return (
+                    <Link key={index} href={item.path} className={iconStyles}>
+                        {item.icon} 
+                    </Link>
+                )
+            })}
+        </div>
+
+        // <div className=' w-full h-34 flex gap-4'>
             
-            <div className=" flex overflow-hidden">
+        //     <div className="flex overflow-hidden">
 
-                <a className="rounded-full flex justify-center text-accent hover:bg-accent" href="http://linkedin.com" target="_blank">
-                    <img className="" src="/assets/icons/linkedin_icon.png" alt="linkedin icon" />
-                </a>
-            </div>
-            <div className="flex overflow-hidden">
+        //         <a className="rounded-full flex justify-center text-accent hover:bg-accent" href="https://www.linkedin.com/in/f-taveras/" target="_blank">
+        //             <FaLinkedin className="" src="/assets/icons/linkedin_icon.png" alt="linkedin icon" />
+        //         </a>
+        //     </div>
+        //     <div className="flex overflow-hidden">
 
-                <a className=" rounded-full flex justify-center text-accent hover:bg-accent" href="https://github.com/f-taveras" target="_blank">
-                    <img className="" src="/assets/icons/github_icon.png" alt="github icon" />
-                </a>
-            </div>
+        //         <a className="rounded-full flex justify-center text-accent hover:bg-accent" href="https://github.com/f-taveras" target="_blank">
+        //             <FaGithub className=""  alt="github icon" />
+        //         </a>
+        //     </div>
             
 
-        </div>)
+        // </div>
+        )
 };
 
 export default Social;
