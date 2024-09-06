ab49f48eabeb4fb1d1e0b753058ae7def8e526c5
Felix A. Taveras <felixataveras1@gmail.com>
2024-09-06 18:31:27 -0400

    finished project

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -42,7 +42,85 @@ import { motion } from "framer-motion"
 
 const Contact = () => {
     return (
-        <div>contac paget</div>
+        
+        <motion.section
+        initial= {{ opacity: 0 }}
+        animate= {{ 
+            opacity: 1, 
+            transition: {
+                delay: 2.4,
+                duration: 0.4,
+                ease: "easeIn"
+            },
+        }}
+        className="py-6"
+        >
+
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row gap-[30px]">
+                    <div className="xl:w-[54%] order-2 xl:order-none">
+                        <form className="flex flex-col gap-6 p-10 bg-[#227272c] rounded-xl"> 
+                            <h3 className="text-4xl text-accent">Let's work together</h3>
+                            <p className="text-white/60">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem minima suscipit magni, itaque sunt consequuntur eius possimus eum expedita dolorem. Quae, excepturi? </p>
+
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                                <Input type="firstname" placeholder=" Firstname" />
+                                <Input type="lastname" placeholder=" Last Name" />
+                                <Input type="email" placeholder=" Email" />
+                                <Input type="phonenumber" placeholder=" Phone Number" />
+                            </div>
+
+                            <Select>
+                                    <SelectTrigger>
+                                <SelectValue className="w-full" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+
+                                        <SelectLabel>Select a service</SelectLabel>
+                                        <SelectItem value="est">Web Development</SelectItem>
+                                        <SelectItem value="cst">Another Service</SelectItem>
+                                        <SelectItem value="mst">Another Service</SelectItem>
+                                    
+                                    </SelectGroup>
+                                </SelectContent>
+
+                                
+                            </Select>
+
+                            <Textarea className="h-[200px]"
+                            placeholder="Type your message here" />
+
+                            <Button size="md" className="max-w-40">Send Message</Button>
+                             </form>
+
+                    </div>
+                    <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+                        
+                        <ul className="flex flex-col gap-10">
+                            {info.map((item,index) => {
+                                return (
+                                    <li key={index} className="flex items-center gap-6">
+                                        <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                                            <div className="text-[28px]">{item.icon}</div>
+                                        </div>
+                                        <div className="flex-1">
+                                            <p className="text-white/60">{item.title}</p>
+                                            <h3 className="text-xl">{item.description}</h3>
+                                        </div>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </motion.section>
     )
 };
 
