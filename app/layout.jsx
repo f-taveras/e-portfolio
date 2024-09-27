5b10ead39120b224d675c58eb812777c47d62d92
Felix A. Taveras <felixataveras1@gmail.com>
2024-09-27 07:54:33 -0400

    added code editor and is giving data back :)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import { JetBrains_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
-// import Backend from "./tutorials/backend";
 import "./globals.css";
 
 // components
--- app/tutorials/[id]/page.jsx
+++ app/tutorials/[id]/page.jsx
@@ -1,5 +1,9 @@
 'use client'
 
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-monokai";
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
@@ -11,6 +15,12 @@ const TutorialDetail = ({ params }) => {
     const [tutorial, setTutorial] = useState(null);
     const [loading, setLoading] = useState(true);
     const router = useRouter();
+    const [ codeEditor, setCodeEditor] = useState();
+
+    const handleCodeEditorChange = (value) => {
+        setCodeEditor(value);
+        console.log(value);
+    }
 
     useEffect(() => {
         if (id) {
@@ -39,17 +49,33 @@ const TutorialDetail = ({ params }) => {
     }
 
     return (
-        <div className='container'>
+        <div className='container flex flex-col xl:flex-row items-center '>
+            <div>
+
             <Button
-                onClick={() => router.back()} 
+                onClick={() => router.back()}
                 className='mb-8'
-            >
+                >
                 Go Back
             </Button>
             <div className='container'>
                 <h1 className='text-2xl mb-8'>{tutorial.title}</h1>
-                <p className='text-white/60'>{tutorial.description}</p>
-            </div>
+                <p className='text-white/60 mb-8'>{tutorial.description}</p>
+                <h1>{codeEditor}</h1>
+                </div>
+                </div>
+                <AceEditor
+                    mode="python"           // Set Python as the language
+                    theme="monokai"         // Choose a theme, e.g., "monokai"
+                    onChange={handleCodeEditorChange}
+                    name="python-code-editor"
+                    editorProps={{ $blockScrolling: true }}
+                    height="500px"
+                    width="80%"
+                    fontSize={16}
+                    showPrintMargin={false}
+                    className=""
+                />
 
         </div>
     );
