8033561fda54198602d7b090d799baeb32129369
Felix A. Taveras <felixataveras1@gmail.com>
2024-09-27 02:54:38 -0400

    merge issues fixed

--- app/tutorials/page.jsx
+++ app/tutorials/page.jsx
@@ -27,5 +27,26 @@ const Tutorials = () => {
     }, []);
 
     return (
+        <div className="flex flex-col items-center">
+            <h1 className="text-xl mb-8">Tutorial List</h1>
+            {data.length > 0 ? (
+                
+                <ul className="lg:min-w-[160vh] xl:min-w-[160vh] ">
+                    {data.map((tutorial, index) => (
+                        <li key={index}>
+                            <Link href={`/tutorials/${tutorial.id}`}>
+                                    {tutorial.number}.{tutorial.title} - {tutorial.difficulty}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No tutorials found</p>
+            )
+                
+            }
+        </div>
+ 
+);
 };
 export default Tutorials;
